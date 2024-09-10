6e1abbcfd83d049f9f0efbf7f53984e404205d63
Amit Dhyani <amitdhyani0334@gmail.com>
2024-09-10 18:57:16 +0530

    added comments

---
+++ 01_var_let_const.js
@@ -0,0 +1,13 @@
+const id = 33;
+let name = "Amit Dhyani";
+var pass = "Amit@0334";
+city = "dharuhera";
+let state;
+
+console.table([id, name, pass, city, state]);
+
+
+
+// if possible try to use let or const over var
+//var stored in global memory | global scope | hoisted | undefined placeholder
+// let and const stored in script memory | block scope | hoisted | undefined placeholder
