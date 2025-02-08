9b52f510d7332ee82a54151f9418a248d714bd78
Amit Dhyani <amitdhyani0334@gmail.com>
2025-02-08 12:00:17 +0530

    modify

--- 04_datatypes2.js
+++ 04_datatypes2.js
@@ -34,7 +34,7 @@ console.log(realName);
 console.log(name);
 
 
-// heap example
+// heap example with object
 let user1 = {name : "Amit Dhyani", age : 23};
 
 let user2 = user1;   // direct refrence to user1
@@ -44,4 +44,9 @@ user2.name = "Dabbi";
 console.log(user1);
 console.log(user2);
 
-
+// heap example with array
+let arr1 = ["Roman", "Greek", "Viking", "Persia"];
+let arr2 = arr1;
+arr2[2] = "Israel";
+console.log(arr1);
+console.log(arr2);
