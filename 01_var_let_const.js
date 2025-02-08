dcc962671c1c1269a5faaebb1843b252f5b82d45
Amit Dhyani <amitdhyani0334@gmail.com>
2025-02-08 11:09:03 +0530

    hoisting

--- 01_var_let_const.js
+++ 01_var_let_const.js
@@ -11,3 +11,14 @@ console.table([id, name, pass, city, state]);
 // if possible try to use let or const over var
 //var stored in global memory | global scope | hoisted | undefined placeholder
 // let and const stored in script memory | block scope | hoisted | undefined placeholder
+
+console.log(age);  // undefined
+var age = 23;
+console.log(age);  // 23
+
+// var give undefined if we access variable before initialization but let const give error
+
+
+
+
+
